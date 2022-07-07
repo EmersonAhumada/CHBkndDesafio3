70f03d83ad53562ef154b9ea00c4e17579f29ec8
Emerson Ahumada <emersonahumada@Emersons-MacBook-Pro.local>
2022-07-07 06:33:59 -0400

    correccion en start

--- index.js
+++ index.js
@@ -10,21 +10,22 @@ const server = app.listen(PORT, () => {
 const products = new container.Contenedor('productos.txt');
 
 app.get('/', (req, res) => {
-    res.send("<h1 style=color:blue>Para visualizar todos los productos ir a la ruta /productos</h1> <h1 style=color:green>Para un producto aleatorio r a la ruta /productoRandom</h1>");
+    res.send("<h1 style=color:blue>Para visualizar todos los productos ir a la ruta /productos</h1> <h1 style=color:green>Para un producto aleatorio ir a la ruta /productoRandom</h1>");
 })
 
 app.get('/productos', async (req, res) => {
-    try {
-    const showProduct = await products.getAll();
-    res.send(showProduct);
-    }catch (error) {console.log (error)}
+  try {
+  const showProduct = await products.getAll();
+  res.send(showProduct);
+  }catch (error) {console.log (error)}
 })
 
+
 app.get('/productoRandom', async (req, res) => {
-    try{
-    const showRandomProduct = await products.getAll();
-    const randomNum = Math.floor(Math.random() * showRandomProduct.length);
-    res.send(showRandomProduct[randomNum]);
-    }
-    catch (error) {console.log (error)}
+  try{
+  const showRandomProduct = await products.getAll();
+  const randomNum = Math.floor(Math.random() * showRandomProduct.length);
+  res.send(showRandomProduct[randomNum]);
+  }
+  catch (error) {console.log (error)}
 });
